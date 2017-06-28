41234587cde20661b38ba271b2f528803056398b
eliLitwack <elilitwack@gmail.com>
2017-06-28 14:41:36 -0400

    adding missing files from parse-yaml commit

--- build.js
+++ build.js
@@ -1,26 +1,89 @@
 var fs = require('fs');
 var md = require('markdown-it')();
-var yamlFront = require('yaml-front-matter')
+var yamlFront = require('yaml-front-matter');
 
-const TOP    = '<!DOCTYPE html><html><head><link rel="stylesheet" href="styles.css"></head><body>\n'
-const BOTTOM = '</body></html>\n'
 
-function writeToFile(stringToWrite){
-  fs.writeFile(__dirname + '/output/site.html', stringToWrite, function(err) {
-    if(err) {
-        return console.log(err);
+
+//boilerplate html that includes code to get css and leaflet javascript files
+var top = fs.readFileSync(__dirname + '/top.html').toString();
+var bottom = fs.readFileSync(__dirname + '/bottom.html').toString();
+
+mdText = fs.readFileSync( __dirname + '/report.md').toString();
+//parse the md file's yaml frontmatter
+var results = yamlFront.loadFront(mdText);
+
+//define the map markdown container with rendering code
+md.use(require('markdown-it-container'), 'map', {
+  //function used to replace the container during markdown parsing
+  render: function (tokens, idx) {
+    if (tokens[idx].nesting === 1) {
+      // opening tag
+      var name = tokens[idx].info.trim().match(/^map\s+(.*)$/)[1]; //returns name portion of the container
+      //i.e. ":::map south-africa" parses to "south-africa"
+
+      //get map attributes from yaml
+      mapInfo     = results['maps'][name]
+      attribution = mapInfo['attribution']
+      imageURL    = mapInfo['image-url']
+      maxZoom     = mapInfo['max-zoom']
+      initialZoom = mapInfo['initial-zoom']
+      latitude    = mapInfo['latitude']
+      longitude   = mapInfo['longitude']
+      accessToken = mapInfo['access-token']
+      height      = mapInfo['height']
+
+      //fill null values with defaults
+      if(!attribution){
+        attribution = "© Development Seed"
+      }
+      if(!latitude){
+        latitude = 0
+      }
+      if(!longitude){
+        longitude = 0
+      }
+      if(!maxZoom){
+        maxZoom = 18
+      }
+      if(!initialZoom){
+        initialZoom = 12
+      }
+      if(!imageURL){
+        imageURL = 'http://a.aerial.openstreetmap.org.za/ngi-aerial/{z}/{x}/{y}.jpg'
+      }
+      if(!height){
+        height = 180
+      }
+
+      //string containing all the code to render the map
+      //css stype describing map size
+      //div and script creating map as described at http://leafletjs.com/examples/quick-start/
+      return '<style>\
+      #' + name + ' { height: ' + height + 'px; }\
+      </style>\
+      <div id=' + name + '></div>' + '<script>\
+      var mymap = L.map("' + name + '").setView([' + latitude + ', ' + longitude + '], ' + initialZoom + ');\
+      L.tileLayer("' + imageURL + '", {\
+      attribution: \'' + attribution + '\',\
+      maxZoom: ' + maxZoom + ',\
+      accessToken: ' + accessToken + '\
+      }).addTo(mymap);\
+      </script>'
+
+    } else {
+      // no closing tag
+      return '';
     }
-  }); 
-};
-
-fs.readFile( __dirname + '/report.md', function (err, data) {
-  if (err) {
-    throw err; 
   }
-  data = data.toString()
-  var results = yamlFront.loadFront(data);
-  html = md.render(results.__content); //results.__content is everying in the md file that isn't frontmatter
-  html = TOP + html + BOTTOM
-  writeToFile(html)
 });
 
+//parse the markdown
+body = md.render(results.__content); //results.__content is everying in the md file that isn't frontmatter
+html = top + body + bottom;
+
+//write the file
+fs.writeFile(__dirname + '/output/site.html', html, function(err) {
+  if(err) {
+      return console.log(err);
+  }
+}); 
--- report.md
+++ report.md
@@ -1,9 +1,19 @@
 ---
-map: world
-url: www.example.com/{z}/{x}/{y}/tile.jpeg
 ---
-
+maps:
+ south-africa:
+  image-url: http://a.aerial.openstreetmap.org.za/ngi-aerial/{z}/{x}/{y}.jpg
+  attribution: Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>
+  max-zoom: 18
+  initial-zoom: 12
+  latitude: -33.9249
+  longitude: 18.4241
+  access-token: 123456
+  height: 180
+---
 # Title
 lorum ipsum
 ## Subheader
 lorum [ipsum](http://www.google.com)
+::: map south-africa
+:::
